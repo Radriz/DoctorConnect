8163bc25f2215f23988e4142aa5e4b811e8f1322
Radriz <Radiksun@list.ru>
2024-05-30 01:47:28 +0400

    commit plan.css and plan.js

--- main.py
+++ main.py
@@ -272,10 +272,10 @@ async def logout(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-    # uvicorn.run("main:app",
-    #             host="0.0.0.0",
-    #             port=443,
-    #             reload=True,
-    #             ssl_keyfile="/etc/letsencrypt/live/drlink.ru/privkey.pem",
-    #             ssl_certfile="/etc/letsencrypt/live/drlink.ru/fullchain.pem")
+    # uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app",
+                host="0.0.0.0",
+                port=443,
+                reload=True,
+                ssl_keyfile="/etc/letsencrypt/live/drlink.ru/privkey.pem",
+                ssl_certfile="/etc/letsencrypt/live/drlink.ru/fullchain.pem")
